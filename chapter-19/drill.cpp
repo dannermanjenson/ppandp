b5162f8f5a747c5641710b50d4cf686fdb1e588a
Ryan Danner <ryan.danner@mykolab.com>
2019-02-19 14:51:06 -0600

    finishing most of the drill

--- chapter-19/drill.cpp
+++ chapter-19/drill.cpp
@@ -6,7 +6,6 @@
 template<typename T> 
 struct S
 {
-
 	S() : val_(0) {}
 
 	S(T initVal)
@@ -15,6 +14,11 @@ struct S
 	}
 
 	~S() { }
+
+	S(const S&)
+	{
+
+	}
     
 	S<T>& operator=(const T& newVal)
 	{
@@ -29,8 +33,33 @@ private:
 	T val_;
 };
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, S<T>& obj)
+{
+	return os << obj.get();
+}
 
-void doDrill()
+template <typename T>
+std::istream& operator>>(std::istream& is, S<T>& obj)
+{
+	// read obj from stream
+	is >> obj.get();
+
+	if ( !is)
+		is.setstate(std::ios::failbit);
+
+	return is;
+}
+
+template <typename T> 
+void read_val(T& v, const std::string& type)
+{
+	std::cout << "Please enter a " << type << std::endl;
+	std::cin >> v;
+	std::cout << "your " << type << " is " << v << std::endl;
+}
+
+void doDrill_1()
 {
 	S<int> my_int(10);
 	S<double> my_double(3.14);
@@ -53,4 +82,32 @@ void doDrill()
 
     my_int= 26;
 	std::cout << my_int.get() << std::endl;
+}
+
+void doDrill_2()
+{
+	S<int> my_int(10);
+	S<double> my_double(3.14);
+	S<char> my_char('a');
+	S<std::string> my_string("Sup dawg");
+	std::vector<int> init_vec{ 10,20,30 };
+	S<std::vector<int>>  my_vec(init_vec);
+
+	std::cout << my_int << std::endl;
+	std::cout << my_double << std::endl;
+	std::cout << my_char << std::endl;
+	std::cout << my_string << std::endl;
+
+	read_val<S<int>>(my_int, "int");
+	read_val<S<double>>(my_double, "double");
+	read_val<S<char>>(my_char, "char");
+	read_val<S<std::string>>(my_string, "string");
+    // figure out this bonus after doing some exercises
+	//read_val<S<std::vector<int>>(my_vec, "vector");
+}
+
+void doDrill()
+{
+	//doDrill_1();
+	doDrill_2();
 }
