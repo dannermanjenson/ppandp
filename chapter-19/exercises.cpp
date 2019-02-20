8862a68e23cb1ae025703a9482331ac90267cb2a
Ryan Danner <ryan.danner@mykolab.com>
2019-02-20 13:57:54 -0600

    adding exercise 3 and 4

--- chapter-19/exercises.cpp
+++ chapter-19/exercises.cpp
@@ -168,9 +168,204 @@ void exercise3()
 	cout << std::hex << ascii_char_to_digit('4', ascii_digit_table) << endl;
 }
 
+// Link class to implement the nodes in a double linked list
+// struct is used to make all variables public for better practice.
+// It also simplifies the writing of the helper functions because
+// I can call the variables directly without having to write an accessor
+// for every one.
+template <typename T>
+struct Link
+{
+	Link(const T& val)
+		: value_(val)
+	{
+	}
+
+	Link(const T& val, Link* p, Link* n)
+	    :value_(val), prev_(p), next_(n)
+	{
+	}
+	~Link() 
+	{
+	}
+
+	void Identify()
+	{
+        cout << value_ << ":0x" << std::hex << this << endl;
+		cout << "\t" << std::hex << "prev_:0x" << prev_ << endl;
+		cout << "\t" << std::hex << "next_:0x" << next_ << endl;
+	}
+
+	T value_;
+	Link* prev_{ nullptr };
+	Link* next_{ nullptr };
+
+private:
+    // hiding the default constructor
+	Link();
+};
+
+// helper function for Link class to print an entire list of nodes
+template <typename T>
+void printList(Link<T>* head)
+{
+	while (head != nullptr)
+	{
+		head->Identify();
+		head = head->next_;
+	}
+}
+
+// Helper function for Link class to insert newLink before node
+// returns newLink if newLink is not nullptr, else returns node
+template <typename T>
+Link<T>* insert(Link<T>* node, Link<T>* newLink)
+{
+	if (node == nullptr) return newLink;
+	if (newLink == nullptr) return node;
+
+	newLink->next_ = node;
+	if (node->prev_ != nullptr)
+	{
+		node->prev_->next_ = newLink;
+	}
+	newLink->prev_ = node->prev_;
+	node->prev_ = newLink;
+	return newLink;
+}
+
+// tests for insert helper function
+void insertTests()
+{
+	cout << "--- Running Link:insert tests ---" << endl;
+	cout << "Output should be Brian, Thor, Josh, Odin" << endl;
+
+    // Odin
+	Link<std::string>* norse_gods = new Link<std::string>( "Odin", nullptr, nullptr );
+
+    // inserting one link by hand
+    // Thor Odin
+	norse_gods = new Link<std::string>("Thor",nullptr, norse_gods);
+	norse_gods->next_->prev_ = norse_gods;
+
+    // inserting link at the end of the list by hand 
+    // Thor Josh Odin
+	insert(norse_gods->next_, new Link<std::string>("Josh"));
+
+    // inserting Brian at the beginning
+    // Brian Thor Josh Odin
+	norse_gods =insert(norse_gods, new Link<std::string>("Brian"));
+	printList(norse_gods);
+}
+
+// helper function for adding a newLink after node
+// returns newLink
+template <typename T>
+Link<T>* add(Link<T>* node, Link<T>* newLink)
+{
+	if (node == nullptr) return newLink;
+	if (newLink == nullptr) return node;
+
+	newLink->prev_ = node;
+
+	if (node->next_ != nullptr)
+	{
+		node->next_->prev_ = newLink;
+	}
+	newLink->next_ = node->next_;
+	node->next_ = newLink;
+	return newLink;
+}
+
+// tests for add helper function
+void addTests()
+{
+	cout << "--- Running Link:add tests ---" << endl;
+	cout << "Output should be Odin, Josh, Thor" << endl;
+
+    // Odin
+	Link<std::string>* norse_gods = new Link<std::string>( "Odin", nullptr, nullptr );
+    
+    // Adding Thor
+    // Odin Thor
+	add(norse_gods, new Link<std::string>("Thor"));
+
+    // Adding josh
+    // Odin Josh Thor
+	add(norse_gods, new Link<std::string>("Josh"));
+	printList(norse_gods);
+}
+
+// helper function to erase nodes from the list
+// returns link before the one erased
+template <typename T>
+Link<T>* erase(Link<T>* node)
+{
+	if (node == nullptr) return nullptr;
+
+
+}
+
+// helper functions to free all memory associated with a list
+template <typename T>
+bool freeLinks(Link<T*> head)
+{
+}
+
+void listOpsTests()
+{
+	//insertTests();
+	addTests();
+}
+
+// Class describing the stats of a god
+class God
+{
+public:
+	God(std::string name,    std::string mythology,
+		std::string vehicle, std::string weapon)
+	    : name_(name),       mythology_(mythology),
+          vehicle_(vehicle), weapon_(weapon)
+	{}
+
+	std::string& getName() { return name_; };
+	std::string& getMythology() { return mythology_; };
+	std::string& getVehicle() { return vehicle_; };
+	std::string& getWeapon() { return weapon_; };
+
+private:
+	std::string name_{ "" };
+	std::string mythology_{ "" };
+	std::string vehicle_{ "" };
+	std::string weapon_{ "" };
+};
+
+// output stream operator for Pair class
+std::ostream& operator<<(std::ostream& os, God god)
+{
+	return os <<         god.getName() << endl
+		      << "\t" << god.getMythology() << endl
+		      << "\t" << god.getVehicle() << endl
+		      << "\t" << god.getWeapon() << endl;
+}
+
+void GodLists()
+{
+	Link<God>* norse_gods= new Link<God>(God{ "Thor", "Norse", "Mjollnir", "Mjollnir" });
+	norse_gods = insert(norse_gods, new Link<God>(God{ "Odin", "Norse", "Sleipner", "Gungnir" }));
+	printList(norse_gods);
+}
+
+void exercise4()
+{
+	GodLists();
+}
+
 void runExercises()
 {
 	//exercise1();
 	//exercise2();
-	exercise3();
+	//exercise3();
+	//exercise4();
+	listOpsTests();
 }
